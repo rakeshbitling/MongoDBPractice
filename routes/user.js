15b12d577cfbce8d687ca6880919ea4c553ac7af
unknown <Rakesh Bitling>
2021-03-29 18:46:14 +0530

    fixed GET error handling

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,9 @@ const UserModel = require('../models/user');
 router.get('/user', async (req, res) => {
     try {
         let u_data = await UserModel.find({});
-        res.send(u_data);    
-    }catch(error) {
-        res.status(500).send({'message' : 'something went wrong'});
+        res.send(u_data);
+    } catch (error) {
+        res.status(500).send({ 'message': 'something went wrong' });
     }
 });
 
@@ -25,6 +25,17 @@ router.post('/user', async (req, res) => {
     } catch (err) {
         res.status(500).send({ 'message': 'Something Went Wrong!' });
     }
+});
+
+router.put('/user', function (req, res) {
+    UserModel.updateOne({ name: "Rakesh" }, { role: "Senior Technical Leader" },
+        function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
 });
 
 module.exports = router;
